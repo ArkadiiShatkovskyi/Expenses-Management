2991a4c1db91daad91fc6bb2ef7a19fd75e34c98
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-03 00:52:39 +0100

    Project set up

--- money_management/test/widget_test.dart
+++ money_management/test/widget_test.dart
@@ -8,12 +8,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:money_management/main.dart';
+import 'package:money_menagment/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+     
     await tester.pumpWidget(MyApp());
+
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
