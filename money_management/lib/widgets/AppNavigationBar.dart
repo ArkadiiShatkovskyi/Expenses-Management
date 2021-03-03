fe70603a239b47299ace4639c175059462a36fe8
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-03 22:21:27 +0100

    Navigation was set up

--- money_management/lib/widgets/AppNavigationBar.dart
+++ money_management/lib/widgets/AppNavigationBar.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:money_menagment/screens/AddExpense.dart';
+import 'package:money_menagment/screens/SummaryScreen.dart';
 
+// ignore: must_be_immutable
 class AppNavigationBar extends StatefulWidget{
 
-  final int itemIndex;
+  int itemIndex;
 
   AppNavigationBar(this.itemIndex);
 
@@ -16,7 +19,7 @@ class _AppNavigationBarState extends State<AppNavigationBar> {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
         currentIndex: widget.itemIndex,
-        onTap: onBNBClick,
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.add),
@@ -30,5 +33,20 @@ class _AppNavigationBarState extends State<AppNavigationBar> {
       );
   }
 
-  void onBNBClick(int value) {}
+  void _onItemTapped(int index) {
+    setState(() {
+      if(index == 0) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => AddExpense()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SummaryScreen()),
+        );
+      }
+      widget.itemIndex = index;
+    });
+  }
 }
