c6dc17d1023fd11c6e27b7bfae6ffa6af638c81f
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-03 22:21:04 +0100

    Fields were added

--- money_management/lib/screens/AddExpense.dart
+++ money_management/lib/screens/AddExpense.dart
@@ -5,92 +5,136 @@ import '../widgets/DatePickerWidget.dart';
 import '../widgets/AppNavigationBar.dart';
 
 class AddExpense extends StatelessWidget{
+
+  double _price = 0.0;
+  String _category = "";
+  String _place = "";
+
   @override
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
         appBar: AppBar(
         centerTitle: true,
-        title: const Text('Summary screen'),
+        title: const Text('Add new expense'),
       ),
       bottomNavigationBar: AppNavigationBar(0),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: const Text("Price"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding:
+                EdgeInsets.only(top: height * 0.025, bottom: width * 0.05),
+                child: const Text(
+                  'Add new record',
+                  style: TextStyle(fontSize: 20),
                 ),
-                TextFormField(
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    decoration: const InputDecoration(
-                      hintText: 'Enter a price',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height * 0.1,
+                    width: width * 0.2,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Text("Price"),
+                  ),
+                  Container(
+                    width: width * 0.4,
+                    height: height * 0.1,
+                    child: TextFormField(
+                        initialValue: _price.toString(),
+                        keyboardType: TextInputType.numberWithOptions(decimal: true),
+                        decoration: const InputDecoration(
+                          hintText: 'Enter a price',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter a price';
+                          }
+                          return null;
+                        },
                     ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a price';
-                      }
-                      return null;
-                    },
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: const Text("Category"),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter a category',
                   ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Choose a category';
-                    }
-                    return null;
-                  },
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: const Text("Place"),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter a place',
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height * 0.1,
+                    width: width * 0.2,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Text("Category"),
                   ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Enter a place';
-                    }
-                    return null;
-                  },
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: const Text("Category"),
-                ),
-                DatePickerWidget(),
-              ],
-            ),
-          ],
+                  Container(
+                    width: width * 0.4,
+                    height: height * 0.1,
+                    child: TextFormField(
+                      initialValue: _category,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter a category',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Choose a category';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height * 0.1,
+                    width: width * 0.2,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Text("Place"),
+                  ),
+                  Container(
+                    width: width * 0.4,
+                    height: height * 0.1,
+                    child: TextFormField(
+                      initialValue: _place,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter a place',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter a place';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height * 0.1,
+                    width: width * 0.2,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Text("Date"),
+                  ),
+                  Container(
+                    width: width * 0.4,
+                    height: height * 0.1,
+                    child: DatePickerWidget(),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
     );
   }
 }
