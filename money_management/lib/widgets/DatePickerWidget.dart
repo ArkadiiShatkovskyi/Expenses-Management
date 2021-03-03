6ef30c1d9b883831a5d2cc67a2deadb8ef48dcb5
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-03 21:36:49 +0100

    DatePickerWidget was created

---
+++ money_management/lib/widgets/DatePickerWidget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DatePickerWidget extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() => _DatePickerWidgetState();
+}
+
+class _DatePickerWidgetState extends State<DatePickerWidget>{
+
+  DateTime _date = DateTime.now();
+
+  void _chooseDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date, // Refer step 1
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (picked != null && picked != _date)
+      setState(() {
+        _date = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _chooseDate(context),
+      child: Text(_date.toString()),
+    );
+  }
+}
