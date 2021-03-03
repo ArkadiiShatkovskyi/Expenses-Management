c84b9f321b5260a5f7dc15338c4a654ba838089c
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-04 00:22:27 +0100

    Saving record was added

--- money_management/lib/screens/AddExpense.dart
+++ money_management/lib/screens/AddExpense.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../widgets/DatePickerWidget.dart';
+import '../models/Expense.dart';
+import '../models/DBProvider.dart';
 import '../widgets/AppNavigationBar.dart';
 
-class AddExpense extends StatelessWidget{
+class AddExpense extends StatefulWidget{
 
-  double _price = 0.0;
-  String _category = "";
-  String _place = "";
+  DBProvider _dbProvider;
+
+  AddExpense(){
+    _dbProvider = DBProvider();
+  }
+
+  @override
+  _AddExpenseState createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  DateTime _date = DateTime.now();
+
+  TextEditingController _tecPrice = new TextEditingController();
+  TextEditingController _tecCategory = new TextEditingController();
+  TextEditingController _tecPlace = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,16 +52,16 @@ class AddExpense extends StatelessWidget{
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: height * 0.1,
+                    height: height * 0.07,
                     width: width * 0.2,
                     padding: const EdgeInsets.only(right: 20),
                     child: const Text("Price"),
                   ),
                   Container(
                     width: width * 0.4,
-                    height: height * 0.1,
+                    height: height * 0.07,
                     child: TextFormField(
-                        initialValue: _price.toString(),
+                        controller: _tecPrice,
                         keyboardType: TextInputType.numberWithOptions(decimal: true),
                         decoration: const InputDecoration(
                           hintText: 'Enter a price',
@@ -66,16 +80,16 @@ class AddExpense extends StatelessWidget{
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: height * 0.1,
+                    height: height * 0.07,
                     width: width * 0.2,
                     padding: const EdgeInsets.only(right: 20),
                     child: const Text("Category"),
                   ),
                   Container(
                     width: width * 0.4,
-                    height: height * 0.1,
+                    height: height * 0.07,
                     child: TextFormField(
-                      initialValue: _category,
+                      controller: _tecCategory,
                       decoration: const InputDecoration(
                         hintText: 'Enter a category',
                       ),
@@ -93,16 +107,16 @@ class AddExpense extends StatelessWidget{
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: height * 0.1,
+                    height: height * 0.07,
                     width: width * 0.2,
                     padding: const EdgeInsets.only(right: 20),
                     child: const Text("Place"),
                   ),
                   Container(
                     width: width * 0.4,
-                    height: height * 0.1,
+                    height: height * 0.07,
                     child: TextFormField(
-                      initialValue: _place,
+                      controller: _tecPlace,
                       decoration: const InputDecoration(
                         hintText: 'Enter a place',
                       ),
@@ -120,21 +134,61 @@ class AddExpense extends StatelessWidget{
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: height * 0.1,
+                    height: height * 0.07,
                     width: width * 0.2,
                     padding: const EdgeInsets.only(right: 20),
                     child: const Text("Date"),
                   ),
                   Container(
                     width: width * 0.4,
-                    height: height * 0.1,
-                    child: DatePickerWidget(),
+                    height: height * 0.07,
+                    child: GestureDetector(
+                      onTap: () => _chooseDate(context),
+                      child: Text(_date.toString().substring(0, 10)),
+                    ),
                   ),
                 ],
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: height * 0.04,
+                      width: width * 0.4,
+                      child: ElevatedButton.icon(onPressed: saveDate, icon: Icon(Icons.save_outlined), label: const Text("Add new record")),
+                    ),
+                  ],
               ),
             ],
           ),
         ),
     );
+  }
+
+  void saveDate(){
+    Expense expense = new Expense(category: _tecCategory.text, place: _tecPlace.text, price: double.parse(_tecPrice.text), date: _date);
+    widget._dbProvider.insertExpense(expense);
+    /** //Data base connection test
+    widget._dbProvider.expenses().then((list) {
+      for(Expense l in list){
+        print("Category: " + l.category + ", Price: " + l.price.toString() + ", Date: " + _date.toString());
+      }
+    });
+   **/
+  }
+
+  void _chooseDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date, // Refer step 1
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (picked != null && picked != _date)
+      setState(() {
+        _date = picked;
+        DateTime dtNow = DateTime.now();
+        _date = _date.add(Duration(hours: dtNow.hour, minutes: dtNow.minute, seconds: dtNow.second, milliseconds: dtNow.millisecond));
+      });
   }
 }
