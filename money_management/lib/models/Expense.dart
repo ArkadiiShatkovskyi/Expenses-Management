9fb445c93869c302daa9e8bff4353c981b56f2c3
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-04 00:22:02 +0100

    To map function was added

--- money_management/lib/models/Expense.dart
+++ money_management/lib/models/Expense.dart
@@ -5,4 +5,13 @@ class Expense{
   final DateTime date;
  
   Expense({this.category, this.place, this.price, this.date});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'category': category,
+      'place': place,
+      'price': price,
+      'date' : date.toString()
+    };
+  }
 }
