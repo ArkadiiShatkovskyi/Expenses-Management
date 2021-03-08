1faa65fc6f4c6ee1fdff1c961ba68e28ced32dcf
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-03-08 22:45:17 +0100

    Setting up new DataElement and set up new GUI

--- money_management/lib/screens/SummaryScreen.dart
+++ money_management/lib/screens/SummaryScreen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 
 import '../widgets/AppNavigationBar.dart';
 import '../widgets/StatisticPieChart.dart';
-import '../widgets/StatisticElement.dart';
+import '../widgets/DataElements.dart';
 
 class SummaryScreen extends StatefulWidget {
   SummaryScreen({Key key}) : super(key: key);
@@ -39,32 +39,42 @@ class _SummaryScreen extends State<SummaryScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              padding:
-                  EdgeInsets.only(top: height * 0.025, bottom: width * 0.05),
-              child: const Text(
-                'Statistic',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            StatisticElement(elementLabel: "Saved", elementValue: 0.0),
-            StatisticElement(elementLabel: "Food", elementValue: 0.0),
-            StatisticElement(elementLabel: "Food delivery", elementValue: 0.0),
-            StatisticElement(elementLabel: "Electronics", elementValue: 0.0),
-            StatisticElement(elementLabel: "Gifts", elementValue: 0.0),
             SizedBox(
-              height: height * 0.1,
+              height: height * 0.05,
+            ),
+            Container(
+                margin: EdgeInsets.only(left: width * 0.05, right: width * 0.05),
+                decoration: BoxDecoration(
+                  backgroundBlendMode: BlendMode.darken,
+                  color: Colors.deepPurple.withAlpha(200),
+                  border: Border.all(
+                    color: Colors.deepPurple.withAlpha(200),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.8),
+                      spreadRadius: 4,
+                      blurRadius: 3,
+                      offset: Offset(7.0, 7.0),
+                    )
+                  ],
+                ),
+                child: DateElements(height: height, width: width,)),
+            SizedBox(
+              height: height * 0.04,
             ),
             Container(
               decoration: BoxDecoration(
                 backgroundBlendMode: BlendMode.darken,
-                color: Colors.purpleAccent.withAlpha(100),
+                color: Colors.deepPurple.withAlpha(200),
                 border: Border.all(
-                  color: Colors.black,
+                  color: Colors.deepPurple.withAlpha(200),
                   width: 1,
                 ),
                 borderRadius: BorderRadius.circular(30),
-
                 boxShadow: [
                   BoxShadow(
                     color: Colors.grey.withOpacity(0.8),
