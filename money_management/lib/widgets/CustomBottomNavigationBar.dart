b74bdc48b2e3fe1ef84b4e02488f839397fe87b2
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-05-02 18:52:19 +0200

    Creating new custom bottom navigation bar

--- money_management/lib/widgets/CustomBottomNavigationBar.dart
+++ money_management/lib/widgets/CustomBottomNavigationBar.dart
@@ -4,21 +4,47 @@ import 'package:money_menagment/screens/AddExpense.dart';
 import 'package:money_menagment/screens/SummaryScreen.dart';
 
 // ignore: must_be_immutable
-class CustomBottomNavigationBar extends StatefulWidget{
-
+class CustomBottomNavigationBar extends StatefulWidget {
   int itemIndex;
 
   CustomBottomNavigationBar(this.itemIndex);
 
   @override
-  _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
 }
 
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  List<bool> _selected = [true, false, false];
+
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.transparent.withAlpha(25),
+      ),
+      child:ToggleButtons(
+        children: <Widget>[
+          Icon(Icons.info, c),
+          Icon(Icons.add),
+          Icon(Icons.settings),
+        ],
+        onPressed: (int index) {
+          setState(() {
+            for (int buttonIndex = 0; buttonIndex < _selected.length; buttonIndex++) {
+              if (buttonIndex == index) {
+                _selected[buttonIndex] = true;
+              } else {
+                _selected[buttonIndex] = false;
+              }
+            }
+          });
+        },
+        isSelected: _selected,
+      ),
+      );
+    /**
     return BottomNavigationBar(
-        backgroundColor: Colors.transparent,
         currentIndex: widget.itemIndex,
         onTap: _onItemTapped,
         items: [
@@ -31,12 +57,12 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
             label: 'Info',
           ),
         ],
-      );
+    ); **/
   }
 
   void _onItemTapped(int index) {
     setState(() {
-      if(index == 0) {
+      if (index == 0) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => AddExpense()),
