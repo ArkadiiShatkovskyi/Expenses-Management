20099f77f2580e3fbe2cc00dc6ef933851ee4951
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-05-02 16:34:48 +0200

    Gradient was set up as background

--- money_management/lib/main.dart
+++ money_management/lib/main.dart
@@ -15,10 +15,42 @@ class MyApp extends StatelessWidget {
       title: 'Money management app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        primarySwatch: Colors.deepPurple,
       ),
-      home: SummaryScreen(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [
+                0.0,
+                0.5,
+                0.7,
+                0.8,
+              ],
+              colors: [
+                Colors.lightGreenAccent.withAlpha(150),
+                Colors.cyanAccent.withAlpha(150),
+                Colors.lightBlue.withAlpha(150),
+                Colors.blue.withAlpha(150),
+                //Colors.blueGrey,
+              ],
+            ),
+          ),
+          child: SummaryScreen(),
+        ),
+      ),
     );
   }
 }
