9950e92a01f03bbea397f4aa19e66e754dd53f9f
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-05-12 22:41:24 +0200

    Drop down menu button was created

---
+++ money_management/lib/widgets/DropDownCategorySelect.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class DropDownCategorySelect extends StatefulWidget {
+  String dropdownValue = 'Other';
+
+  String getValue() {
+    return dropdownValue;
+  }
+
+  @override
+  State<DropDownCategorySelect> createState() => _DropDownCategorySelectState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _DropDownCategorySelectState extends State<DropDownCategorySelect> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: widget.dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.black, fontSize: 18),
+      underline: Container(
+        height: 2,
+        color: Colors.black.withAlpha(75),
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          widget.dropdownValue = newValue;
+        });
+      },
+      items: <String>[
+        'Other',
+        'Food',
+        'Electronic',
+        'Vape',
+        'Clothes',
+        "Food delivery"
+      ].map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
