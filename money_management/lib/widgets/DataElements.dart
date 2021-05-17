b3cc5f2fd137cd92092e8560eb7187090aed682c
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-05-17 22:18:28 +0200

    Dynamic data was set up

--- money_management/lib/widgets/DataElements.dart
+++ money_management/lib/widgets/DataElements.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'StatisticElement.dart';
 
-class DateElements extends StatelessWidget{
-
+class DateElements extends StatelessWidget {
   final double height;
   final double width;
+  final Map<String, double> dataMap;
 
-  DateElements({this.height, this.width});
+  DateElements({this.height, this.width, this.dataMap});
 
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [
+      children: [
         Container(
           padding: EdgeInsets.only(bottom: width * 0.05),
           child: const Text(
@@ -19,15 +19,24 @@ class DateElements extends StatelessWidget{
             style: TextStyle(fontSize: 20, color: Colors.black),
           ),
         ),
-          StatisticElement(elementLabel: "Saved", elementValue: 0.0),
-          StatisticElement(elementLabel: "Food", elementValue: 0.0),
-          StatisticElement(elementLabel: "Food delivery", elementValue: 0.0),
-          StatisticElement(elementLabel: "Electronics", elementValue: 0.0),
-          StatisticElement(elementLabel: "Gifts", elementValue: 0.0),
-          SizedBox(height: height * 0.02),
-        ],
+        for (String item in dataMap.keys) getStatisticElement(item),
+        //StatisticElement(elementLabel: "Saved", elementValue: 0.0),
+        //StatisticElement(elementLabel: "Food", elementValue: 0.0),
+        //StatisticElement(elementLabel: "Food delivery", elementValue: 0.0),
+        //StatisticElement(elementLabel: "Electronics", elementValue: 0.0),
+        //StatisticElement(elementLabel: "Gifts", elementValue: 0.0),
+        SizedBox(height: height * 0.02),
+      ],
     );
   }
 
+  Widget getStatisticElement(String keyData) {
+    double valueData = 0.0;
+    dataMap.forEach((key, value) {
+      if (key == keyData) {
+        valueData = value;
+      }
+    });
+    return StatisticElement(elementLabel: keyData, elementValue: valueData);
+  }
 }
-
