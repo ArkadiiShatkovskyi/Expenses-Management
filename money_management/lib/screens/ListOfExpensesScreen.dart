905576b565d034956769d6431e7ba84a774d38b0
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2021-05-18 23:57:46 +0200

    New screen UI for empty data was created

--- money_management/lib/screens/ListOfExpensesScreen.dart
+++ money_management/lib/screens/ListOfExpensesScreen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import '../widgets/CustomContainer.dart';
+import '../models/Expense.dart';
+import '../models/DBProvider.dart';
 import '../widgets/DropDownCategorySelect.dart';
 import '../widgets/CustomAppBar.dart';
 import '../widgets/GradientBackground.dart';
 
 class ListOfExpensesScreen extends StatefulWidget {
+  final DBProvider _dbProvider = DBProvider();
   final Widget bottomNavigationBar;
   final DropDownCategorySelect categorySelect = DropDownCategorySelect();
+  List<Expense> listOfExpenses = [];
 
   ListOfExpensesScreen(this.bottomNavigationBar);
 
@@ -28,7 +33,41 @@ class _ListOfExpensesScreenState extends State<ListOfExpensesScreen> {
         width: width,
         height: height,
         child: GradientBackGround(
-          child: Column(
+          child:
+          widget.listOfExpenses.length == 0 ?
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: height * 0.04,
+              ),
+              CustomAppBar(
+                width: width,
+                height: height * 0.1,
+                text: "List of expenses",
+              ),
+              SizedBox(
+                height: height * 0.3,
+              ),
+              CustomContainer(
+                width: width,
+                height: height,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: const Text(
+                    "There is no data. Add new data on another screen.",
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.384,
+              ),
+              widget.bottomNavigationBar,
+            ],
+          )
+              :
+          Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               SizedBox(
@@ -42,6 +81,7 @@ class _ListOfExpensesScreenState extends State<ListOfExpensesScreen> {
               SizedBox(
                 height: height * 0.05,
               ),
+
               SizedBox(
                 height: height * 0.115,
               ),
@@ -51,5 +91,13 @@ class _ListOfExpensesScreenState extends State<ListOfExpensesScreen> {
         ),
       ),
     );
+  }
+
+  void getData() {
+    widget._dbProvider.expenses().then((list) {
+      setState(() {
+        widget.listOfExpenses = list;
+      });
+    });
   }
 }
