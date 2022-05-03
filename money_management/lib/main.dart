86397114d6f8eff4e7c0ddf6427e5bc3fbf83513
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-05-03 21:21:56 +0200

    Decoration for Container was deleted

--- money_management/lib/main.dart
+++ money_management/lib/main.dart
@@ -29,25 +29,6 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [
-                0.0,
-                0.5,
-                0.7,
-                0.8,
-              ],
-              colors: [
-                Colors.lightGreenAccent.withAlpha(150),
-                Colors.cyanAccent.withAlpha(150),
-                Colors.lightBlue.withAlpha(150),
-                Colors.blue.withAlpha(150),
-                //Colors.blueGrey,
-              ],
-            ),
-          ),
           child: SummaryScreen(null),
         ),
       ),
