1a2b710f0faf72e4e1e2e27a9e4a42adee0090af
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2022-05-03 21:19:00 +0200

    Box shadow was added

--- money_management/lib/widgets/CustomContainer.dart
+++ money_management/lib/widgets/CustomContainer.dart
@@ -12,7 +12,16 @@ class CustomContainer extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(left: width * 0.05, right: width * 0.05),
       decoration: BoxDecoration(
-        color: Colors.transparent.withAlpha(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: Offset(0, 11), // changes position of shadow
+          ),
+        ],
+        // color: Colors.transparent.withAlpha(25),
+        color: Colors.indigoAccent.withAlpha(175),
         borderRadius: BorderRadius.circular(30),
       ),
       child: this.child,
