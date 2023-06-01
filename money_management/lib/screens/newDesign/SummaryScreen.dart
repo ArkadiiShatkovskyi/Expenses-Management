ef6d87a33e47196e07409ee36b89028b01834299
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2023-06-01 19:21:25 +0200

    Template Design

--- money_management/lib/screens/newDesign/SummaryScreen.dart
+++ money_management/lib/screens/newDesign/SummaryScreen.dart
@@ -15,73 +15,80 @@ class _SummaryScreenState extends State<SummaryScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Row(
-        children: [
-          Container(
-            height: height * 0.25,
-            width: width,
-            child: Row(
-              children: [
-                const Text(
-                  "My Expenses",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.only(top:50),
+        child: Row(
+          children: [
+            Container(
+              height: height * 0.25,
+              //width: width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "My Expenses",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const Text(
-                  'Summary (private)',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.normal,
+                  const Text(
+                    'Summary (private)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 13,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Container(
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          const Icon(
-                            Icons.calendar_today_sharp,
-                            color: Colors.grey,
-                            size: 15,
-                          ),
-                          Container(
-                            child: Row(
-                              children: [
-                                const Text(
-                                  "07 Feb, 2019",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const Text(
-                                  "18% more than last month",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
-                              ],
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10),
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, right: 10,),
+                              child: const Icon(
+                                Icons.calendar_today_sharp,
+                                color: Colors.grey,
+                                size: 20,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                            Container(
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    "07 Feb, 2019",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const Text(
+                                    "18% more than last month",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
